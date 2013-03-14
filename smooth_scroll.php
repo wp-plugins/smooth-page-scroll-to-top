d0c60474e678c71d426ad4827a7bd2eab6c3da6f
DarkWolf <DarkWolf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-14 14:23:28 +0000

    New minor release!
    git-svn-id: https://plugins.svn.wordpress.org/smooth-page-scroll-to-top/tags/0.2@681818 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- smooth_scroll.php
+++ smooth_scroll.php
@@ -5,34 +5,36 @@ Plugin URI: http://wordpress.org/extend/plugins/smooth-page-scroll-to-top/
 Description: Simple Smooth Page Scroll "to Top" - Original script by: <a href="http://gazpo.com/2012/02/scrolltop/" title="Source Code">http://gazpo.com/</a> - Plugin Packed by: <a href="http://www.darkwolf.it/" title="Plugin Author">Salvatore Noschese - (DarkWolf)</a>
 You can see in action here: http://vegamami.altervista.org/ :)
 Author: Salvatore Noschese - DarkWolf
-Version: 0.1
+Version: 0.2
 Author URI: http://www.darkwolf.it/
 */
 
 // Prevent Direct Access with homepage redirect
-if (!defined('DB_NAME')) {
-	header('Location: ../../../');
+if (!defined('DB_NAME'))
+{
+	header('Location: http://'.$_SERVER['SERVER_NAME'].'/');
 }
 
 // Function Load jQuery
-function smooth_scrool_jquery() {
-	if (!is_admin()) {
-		wp_enqueue_script('jquery');
-	}
+function smooth_scrool_jquery()
+{
+	wp_enqueue_script('jquery');
 }
 
-// Function Add in Head
-function smooth_scrool_head() {
-	$smooth_scrool_lnk = WP_PLUGIN_URL.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__));
-		echo "\n<!-- Smooth Scroll by DarkWolf -->
-<script type=\"text/javascript\" src=\"".$smooth_scrool_lnk."files/smooth_scroll.js\"></script>
-<style type=\"text/css\">@import url('".$smooth_scrool_lnk."files/smooth_scroll.css');</style>
+// Function Add in Header
+function smooth_scrool_head()
+{
+	$sc_files = plugin_dir_url(__FILE__)."files/smooth_scroll.";
+	echo "\n<!-- Smooth Scroll by DarkWolf -->
+<script type=\"text/javascript\" src=\"".$sc_files."js\"></script>
+<style type=\"text/css\">@import url('".$sc_files."css');</style>
 <!-- Smooth Scroll by DarkWolf -->\n\n";
 }
 
 // Function Add in Footer
-function smooth_scrool_footer() {
-		echo "\n<!-- Smooth Scroll by DarkWolf --><a href=\"#\" class=\"scrollup\">Scroll</a><!-- Smooth Scroll by DarkWolf -->\n";
+function smooth_scrool_footer()
+{
+	echo "\n<!-- Smooth Scroll by DarkWolf --><a href=\"#\" class=\"scrollup\">Scroll</a><!-- Smooth Scroll by DarkWolf -->\n";
 }
 
 // Run All Function
